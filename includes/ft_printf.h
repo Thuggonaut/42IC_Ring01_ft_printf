fe7074a670656c0bb69929f607864c117570a5c8
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-05-30 21:09:45 +0930

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,16 +1,16 @@
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef FT_PRINTF_H //These directives are used to prevent multiple inclusions of the header file
+# define FT_PRINTF_H //They check if the FT_PRINTF_H macro is not defined and then define it, ensuring that the content within the #ifndef and #endif blocks is processed only once
 
-# include <stdlib.h>
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdio.h>
+# include <stdlib.h> //Provides functions for dynamic memory allocation, process control, and other general-purpose utilities
+# include <stdarg.h> //Contains macros and functions to work with variadic arguments
+# include <unistd.h> //Provides access to various operating system services, including I/O operations such as write()
+# include <stdio.h> //Provides functions for standard input/output operations
 
-int		ft_printf(const char *format, ...);
-int		ft_handle_char(va_list ap);
-int		ft_handle_str(va_list ap);
-int		ft_handle_int(va_list ap);
-int		ft_handle_uns(va_list ap);
+int		ft_printf(const char *format, ...); //The recreation of printf(). Takes in a format string, and a variable number of arguments
+int		ft_handle_char(va_list ap); //Handles the %c format specifier
+int		ft_handle_string(va_list ap); //Handles the %s format specifier
+int		ft_handle_int(va_list ap); //Handles the %d and %i format specifiers
+int		ft_handle_unsigned(va_list ap); //Handles the %u format specifier
 int		ft_handle_pointer(va_list ap);
 int		ft_handle_hexlower(va_list ap);
 int		ft_handle_hexupper(va_list ap);
