bce21063fb58ac26b17be8eff0dcb8fe64842d8a
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-05-30 21:13:28 +0930

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -11,12 +11,12 @@ int		ft_handle_char(va_list ap); //Handles the %c format specifier
 int		ft_handle_string(va_list ap); //Handles the %s format specifier
 int		ft_handle_int(va_list ap); //Handles the %d and %i format specifiers
 int		ft_handle_unsigned(va_list ap); //Handles the %u format specifier
-int		ft_handle_pointer(va_list ap);
-int		ft_handle_hexlower(va_list ap);
-int		ft_handle_hexupper(va_list ap);
-int	  ft_handle_percent(void);
-size_t  ft_strlen(const char *s);
-int		ft_putchar(char c);
-int		ft_putnbr_base(unsigned long long nb, const char *base);
+int		ft_handle_pointer(va_list ap); //Handles the %p format specifier
+int		ft_handle_hexlower(va_list ap); //Handles the %x format specifier for lowercase hexadecimal output
+int		ft_handle_hexupper(va_list ap); //Handles the %X format specifier for uppercase hexadecimal output
+int	  ft_handle_percent(void); //Handles the %% format specifier for printing a percent sign
+size_t  ft_strlen(const char *s); //Calculates the length of a string
+int		ft_putchar(char c); //Writes a single character to the standard output
+int		ft_putnbr_base(unsigned long long nb, const char *base); //Writes a number in a specified base to the standard output
 
-#endif
+#endif //This directive marks the end of the conditional inclusion block, closing the #ifndef directive at the beginning of the file
