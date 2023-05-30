7e878846ee11e299d9737715e78e3266d809ce1d
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-05-31 00:16:07 +0930

    Update ft_handle_char.c

--- src/ft_handle_char.c
+++ src/ft_handle_char.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-int	ft_handle_char(va_list ap) //`ap` is a v`a_list` pointer that is passed as an argument. Inside the function, this pointer is used to access the variable arguments
+int	ft_handle_char(va_list ap) //`ap` is a `va_list` pointer that is passed as an argument. Inside the function, this pointer is used to access the variable arguments
 {
 	char	c; //To store the character argument that will be printed
 
