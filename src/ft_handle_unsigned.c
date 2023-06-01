fd18282b438f48e47a001d425ffb6b2bc571ef6f
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-06-01 23:16:35 +0930

    Update ft_handle_unsigned.c

--- src/ft_handle_unsigned.c
+++ src/ft_handle_unsigned.c
@@ -1,9 +1,9 @@
 #include "ft_printf.h"
 
-int	ft_handle_unsigned(va_list ap)
+int	ft_handle_unsigned(va_list ap) //`ap` is a `va_list` pointer that is passed as an argument. Inside the function, this pointer is used to access the variable arguments
 {
-	unsigned int	n;
+	unsigned int	n; //To store the value of the next `unsigned int` argument retrieved from `ap` for processing
 
-	n = va_arg(ap, unsigned int);
-	return (ft_putnbr_base(n, "0123456789"));
+	n = va_arg(ap, unsigned int); //Retrieves the next `unsigned int` argument from `ap` and assigns it to `n`
+	return (ft_putnbr_base(n, "0123456789")); //Prints the value of `n` in base 10 (decimal) and returns the number of characters printed
 }
