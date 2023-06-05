fe0eb701be963abcb7ca4e18a693ec6d0c07c09c
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-06-05 20:51:51 +0930

    Update ft_printf.c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -21,53 +21,29 @@ static int	ft_specifier(char c, va_list ap) //Takes a character `c`, representin
 	return (write(1, &c, 1)); //If the format specifier `c` doesn't match any of the specifiers in `ft_specifier()`, print the character `c` as it is to the standard output, and returns the total number of characters printed
 }
 
-int ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...) //takes a format string `format` as its first argument, followed by a variable number of arguments indicated by the ellipsis `...`
 {
-	va_list ap;
-	int count;
-
-	if (!format)
-		return (0);
-	va_start(ap, format);
-	count = 0;
-	while (*format)
-	{
-		if (*format == '%' && !*(format + 1))
-			break ;
-		else if (*format == '%')
-			count += ft_specifier(*(++format), ap);
-		else
-			count += write(1, format, 1);
-		format++;
-	}
-	va_end(ap);
-	return (count);
-}
-
-
-int	ft_printf(const char *format, ...) //takes a format string `format` as its first argument, followed by a variable number of arguments indicated by the ellipsis `...`
-{
-	va_list	ap; //`ap` is an argument pointer and will be used to access the variable arguments
-	int		i; //Used as a loop counter to iterate through each character of the format string
-	int		count; //Keeps track of the total number of characters printed
+	va_list ap; //`ap` is an argument pointer and will be used to access the variable arguments
+	int count; //Keeps track of the total number of characters printed
 
 	if (!format) //Checks if the format string is `NULL`. If it is, the function returns `0`, indicating that no characters were printed
 		return (0);
 	va_start(ap, format); //`ap` is initialised by `va_start()` macro, which prepares `ap` to access the variable arguments passed to `ft_printf()`
-	i = -1; //`-1` not `0` because it will be incremented before accessing the first character in the string. This allows the loop to start from `index 0` when `format[++i]` is first evaluated
 	count = 0;
-	while (format[++i]) //Loop through each character of the format string until it reaches the end of the string. See #2
+	while (*format) //Loop through each character of the format string until it reaches the end of the string. See #2
 	{
-		if (format[i] == '%' && !format[i + 1]) //Checks if the current character is a '%' sign and the next character is '\0'. If true, it means there is a single '%' sign at the end of the format string, indicating an incomplete format specifier
+		if (*format == '%' && !*(format + 1)) //Checks if the current character is a '%' sign and the next character is '\0'. If true, it means there is a single '%' sign at the end of the format string, indicating an incomplete format specifier
 			break ; //Terminates the loop prematurely, and continues with the code that follows the loop (in this case, it's the `va_end(ap)` and `return (count)`
-		else if (format[i] == '%') //Checks if the current character in the `format` string is a '%' sign, indicating a format specifier is found
-			count += ft_specifier(format[++i], ap); //Calls `ft_specifier()` with the correct format specifier and adds the return value to `count`, which keeps track of the total number of characters printed.See #3
+		else if (*format == '%') //Checks if the current character in the `format` string is a '%' sign, indicating a format specifier is found
+			count += ft_specifier(*(++format), ap); //Calls `ft_specifier()` with the correct format specifier and adds the return value to `count`, which keeps track of the total number of characters printed.See #3
 		else
-			count += write(1, &format[i], 1); //If none of the previous conditions are met, the current character is a regular character. It is written to the standard output, and the return value `1`, (indicating success) is added to `count`
+			count += write(1, format, 1); //If none of the previous conditions are met, the current character is a regular character. It is written to the standard output, and the return value `1`, (indicating success) is added to `count`
+		format++; //Move to the next character in the format string fpr processing
 	}
 	va_end(ap); //Cleans up `ap` using the `va_end` macro
 	return (count); //Returns the total number of characters printed, which is stored in `count`
 }
+
 /*
 #1	This is a static function called `ft_specifier` and is responsible for handling different format specifiers and returning the number of characters printed.
 		- A static function is a function that has a "static" storage class specifier in its declaration. 
